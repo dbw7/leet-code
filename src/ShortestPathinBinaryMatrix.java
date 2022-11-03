c4c73ebf5113a599b1a370de3010e28cd021e876
dbw7 <90856056+dbw7@users.noreply.github.com>
2022-11-03 02:21:00 -0400

    Initial commit

---
+++ src/ShortestPathinBinaryMatrix.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestPathinBinaryMatrix {
+    class Solution{
+        class State{
+            int row;
+            int col;
+            int steps;
+            State(int row, int col, int steps){
+                this.row = row;
+                this.col = col;
+                this.steps = steps;
+            }
+        }
+        int n;
+        int[][] directions = new int[][]{{-1, -1}, {1,1}, {1,0}, {0,1},{-1,0}, {0,-1},{1,-1},{-1,1}};
+
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            if(grid[0][0] == 1){
+                return -1;
+            }
+            n = grid.length;
+
+            boolean[][] seen = new boolean[n][n];
+            seen[0][0] = true;
+
+            Queue<State> queue = new LinkedList<>();
+            queue.add(new State(0, 0, 1));
+
+            while(!queue.isEmpty()){
+                State state = queue.remove();
+                int row = state.row;
+                int col = state.col;
+                int steps = state.steps;
+                if(row == n-1 && col == n-1){
+                    return steps;
+                }
+                for(int[] direction: directions){
+                    int nextRow = row + direction[0], nextCol = col + direction[1];
+                    if(valid(nextRow, nextCol, grid ) && !seen[nextRow][nextCol]){
+                        seen[nextRow][nextCol] = true;
+                        queue.add(new State(nextRow, nextCol, steps + 1));
+                    }
+                }
+
+            }
+            return -1;
+        }
+        public boolean valid(int row, int col, int[][] grid){
+            return 0 <= row && row < n && 0 <= col && col < n && grid[row][col] == 0;
+        }
+    }
+}
